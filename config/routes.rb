916ac0ebccc55c48c3354a27035721aa40d7ad0f
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-04-05 14:42:17 +0000

    [Start & Finish] 3-8 topページ,bookのindexページのレイアウト作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :books
 
   # アプリケーションのトップ画面を「/」で表示させる
   get '/', to: 'homes#index'
   # ルートパス
   root :to => 'homes#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
