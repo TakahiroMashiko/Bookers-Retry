3fa9a7e59f56d9b8e5507f5d67ae7912e92e3a0d
TakahiroMashiko <takahiromashiko3122@gmail.com>
2021-04-05 14:47:42 +0000

    [Start & Finish] 3-8 bookの新規登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,60 @@
 class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    # 投稿処理の成功後、サクセスメッセージを表示
+    if @book.save
+      redirect_to book_path(@book), notice: "Book was successfully created."
+    else
+      @books=Book.all
+      render 'index'
+    end
   end
 
   def show
-  end
-
-  def new
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    # 更新処理の成功後、サクセスメッセージを表示
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "Book was successfully updated."
+    else
+      @books=Book.all
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    # 削除処理の成功後、サクセスメッセージを表示
+    if @book.destroy
+      redirect_to books_path(@book), notice: "Book was successfully destroyed."
+      # flash[:notice] = "Book was successfully destroyed." !この書き方でもOK!
+      # redirect_to books_path(@book) !この書き方でもOK!
+    end
+  end
+
+# ストロングパラメータを設定
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
